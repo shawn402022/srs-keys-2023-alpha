eed0320d53d14c9ac1e3b319c5f1804d3144fe70
Shawn <sgiovanni1@yahoo.com>
2023-06-27 08:19:45 -0400

    refactored some more and added note name to keys to show in the browser

--- main2.js
+++ main2.js
@@ -16,56 +16,124 @@ const range = ['C2', 'C7'];
 
 
 
-const piano = document.querySelector('#piano');
+
 
 const app = {
   setupPiano() {
+    const piano = document.querySelector('#piano');
     const allNaturalNotes = this.getAllNaturalNotes(range);
     const pianoWidth = allNaturalNotes.length * whiteKeyWidth;
 
     const SVG = this.createMainSVG(pianoWidth, pianoHeight)
-    // add main SVG to paino div
-    piano.appendChild(SVG)
+
     
 
     // add white Keys
     let whiteKeyPositionX = 0;
-    for(let i = 0; i < allNaturalNotes.length; i++) {
+
+    allNaturalNotes.forEach((noteName) => {
+      const whiteKeyTextGroup = utils.createSVGElement("g");
       const whiteKey = this.createKey({ className: "white-key", width: whiteKeyWidth, height: pianoHeight});
-      whiteKey.setAttribute("x", whiteKeyPositionX);
-        whiteKey.setAttribute('data-note-name', allNaturalNotes[i])
+      const text = utils.createSVGElement('text');
+
+      utils.addTextContent(text, noteName);
+      utils.setAttributes(whiteKeyTextGroup, {'width': whiteKeyWidth});
+      utils.setAttributes(text, {
+        "x": whiteKeyPositionX + whiteKeyWidth / 2,
+        'y': 380,
+        "text-anchor": "middle"
+      });
+      utils.setAttributes(whiteKey, {
+        'x': whiteKeyPositionX,
+        "data-note-name": noteName,
+
+      });
+
+      text.classList.add('white-key-text')
+      whiteKeyTextGroup.appendChild(whiteKey);
+      whiteKeyTextGroup.appendChild(text);
+      SVG.appendChild(whiteKeyTextGroup)
+
+
+
+      // increment spacing between white keys
       whiteKeyPositionX += whiteKeyWidth;
-      SVG.appendChild(whiteKey)
-    }
+      
+
+    });
+
 
     //add black keys
     let blackKeyPositionX = 60;
     allNaturalNotes.forEach((naturalNote, index, array) => {
-      const blackKeY = this.createKey( {className: 'black-key', width: whiteKeyWidth /2, height: pianoHeight /1.6})
-      blackKeY.setAttribute('x', blackKeyPositionX);
+      // if last iteration of keys, do not add black key
+      if(index === array.length - 1) {
+        return;
+      }
+
+
+
+
+      const blackKeyTextGroup = utils.createSVGElement("g");
+      const blackKey = this.createKey( {className: 'black-key', width: whiteKeyWidth /2, height: pianoHeight /1.6})
+      const flatNameText =  utils.createSVGElement("text")
+      const sharpNameText = utils.createSVGElement("text")
+
+      utils.setAttributes(blackKeyTextGroup, {"width": whiteKeyWidth / 2});
+
 
       for(let i = 0; i < naturalNotesSharps.length; i++) {
         let naturalSharpNoteName = naturalNotesSharps[i];
         let naturalFlatNoteName = naturalNotesFlats[i];
+
         if (naturalSharpNoteName === naturalNote[0]) {
-          blackKeY.setAttribute('data-sharp-name', `${naturalSharpNoteName}# ${ naturalNote[1]}`);
-          blackKeY.setAttribute('data-flat-name', `${naturalFlatNoteName}b ${ naturalNote[1]}`);
+
+          utils.setAttributes(blackKey, {
+            'x': blackKeyPositionX,
+            'data-sharp-name': `${naturalSharpNoteName}# ${ naturalNote[1]}`,
+            'data-flat-name': `${naturalFlatNoteName}b ${ naturalNote[1]}`
+
+          });
+
+          utils.setAttributes(sharpNameText, {
+            "text-anchor": "middle",
+            'y':215,
+            'x': blackKeyPositionX + (whiteKeyWidth / 4)
+          });
+
+          utils.setAttributes(flatNameText, {
+            "text-anchor": "middle",
+            'y':235,
+            'x': blackKeyPositionX + (whiteKeyWidth / 4)
+
+          })
+
+          utils.addTextContent(sharpNameText, `${ naturalSharpNoteName }♯`);
+          utils.addTextContent(flatNameText, `${ naturalFlatNoteName }♭`);
+
+          flatNameText.classList.add("black-key-text");
+          sharpNameText.classList.add("black-key-text");
+
           // add double spacing between D# and A#
           if(naturalSharpNoteName === "D" || naturalSharpNoteName === "A") {
             blackKeyPositionX += whiteKeyWidth * 2;
           } else {
             blackKeyPositionX += whiteKeyWidth;
           }
-          // if last iteration of keys, do not add black key
-          if(index !== array.length - 1) {
-            SVG.appendChild(blackKeY)
-          }
-          
+
+          blackKeyTextGroup.appendChild(blackKey)
+          blackKeyTextGroup.appendChild(flatNameText)
+          blackKeyTextGroup.appendChild(sharpNameText)
+
         }
+        
 
       }
+      SVG.appendChild(blackKeyTextGroup)
 
-    })
+    });
+    // add main SVG to paino div
+    piano.appendChild(SVG)
 
  
   },
@@ -82,8 +150,12 @@ const app = {
   createKey({ className, width, height }) {
     const key = utils.createSVGElement('rect');
     key.classList.add(className);
-    key.setAttribute('width', width);
-    key.setAttribute('height', height);
+
+    utils.setAttributes(key, {
+      'width': width,
+      'height': height
+    })
+ 
     return key;
   },
 
@@ -137,11 +209,18 @@ const app = {
   createMainSVG(pianoWidth, pianoHeight) {
     const svg = utils.createSVGElement("svg");
 
-    svg.setAttribute("width", "100%");
-    svg.setAttribute("version", "1.1");
-    svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-    svg.setAttribute("xmlns:xlink", "http://www.w3.org/1999/xlink");
-    svg.setAttribute("viewBox", `0 0 ${pianoWidth} ${pianoHeight}`);
+
+    utils.setAttributes(svg, {
+
+      "width": "100%",
+      "version": "1.1",
+      "xmlns": "http://www.w3.org/2000/svg",
+      "xmlns:xlink": "http://www.w3.org/1999/xlink",
+      "viewBox": `0 0 ${pianoWidth} ${pianoHeight}`
+      
+
+    })
+
 
     return svg;
 
@@ -153,6 +232,14 @@ const utils = {
     const element = document.createElementNS('http://www.w3.org/2000/svg', el);
     return element;
   },
+  setAttributes(el, attrs) {
+    for (let key in attrs)
+    el.setAttribute(key, attrs[key])
+  },
+
+  addTextContent(el, content) {
+    el.textContent = content
+  }
 };
 
 app.setupPiano();
